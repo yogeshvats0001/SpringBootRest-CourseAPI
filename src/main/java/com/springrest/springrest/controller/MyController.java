c6cfe944105bd98811c7e259ac9a67a51eb0e23c
Yogesh Vats <yogisharma1062@gmail.com>
2023-03-05 02:30:51 +0530

    Adding ResponseEntity<HttpStatus> in Controller

--- src/main/java/com/springrest/springrest/controller/MyController.java
+++ src/main/java/com/springrest/springrest/controller/MyController.java
@@ -3,8 +3,10 @@ package com.springrest.springrest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,8 +49,13 @@ public class MyController {
 	
 	//delete the course
 	@DeleteMapping("/courses/{courseID}")
-	public String deleteCourse(@PathVariable String courseID) {
-		return this.courseService.deleteCourse(Long.parseLong(courseID));
+	public ResponseEntity<HttpStatus> deleteCourse(@PathVariable String courseID) {
+		try {
+			this.courseService.deleteCourse(Long.parseLong(courseID));
+			return new ResponseEntity<>(HttpStatus.OK);
+		}catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 	
 	//updating the course
